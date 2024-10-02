c4d57a6c3c963e05b8b581d4d8ea9acba112bc5e
OliverEC <oliverec04@gmail.com>
2024-10-03 00:01:30 +0200

    ugly side menu

--- vite-react-rtk-template/src/state/store.ts
+++ vite-react-rtk-template/src/state/store.ts
@@ -1,11 +1,13 @@
-﻿import {configureStore} from '@reduxjs/toolkit'
-import counterReducer from "./features/counter/counterSlice.ts";
+﻿import {configureStore} from '@reduxjs/toolkit';
+import layoutReducer from './features/layout/layoutSlice.ts';
+import counterReducer from './features/counter/counterSlice.ts';
 
 export const store = configureStore({
-    reducer: {
-        counter: counterReducer
-    },
-})
+	reducer: {
+		layout: layoutReducer,
+		counter: counterReducer
+	},
+});
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
