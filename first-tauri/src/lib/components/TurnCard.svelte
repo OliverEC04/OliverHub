b74e120fee001e414500e55d7b7b3219bb3f1648
OliverEC <oliverec04@gmail.com>
2025-03-13 16:34:06 +0100

    cards

---
+++ first-tauri/src/lib/components/TurnCard.svelte
@@ -0,0 +1,17 @@
+﻿<script lang="ts">
+	interface Props {
+		id: number;
+		onClick(id: number): void;
+	}
+
+	let {id, onClick}: Props = $props();
+</script>
+
+<button onclick={() => onClick(id)}>TurnCard {id}</button>
+
+<style lang="scss">
+  button {
+    width: 80px;
+    height: 80px;
+  }
+</style>
---
+++ first-tauri/src/lib/utils/arrayHelpers.ts
@@ -0,0 +1,15 @@
+﻿export const shuffle = function<T>(array: T[]): T[] {
+	const copyArray = array.slice();
+	let currentIndex = copyArray.length;
+
+	while (currentIndex !== 0) {
+		let randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+
+		[copyArray[currentIndex], copyArray[randomIndex]] = [
+			copyArray[randomIndex], copyArray[currentIndex]
+		];
+	}
+
+	return copyArray;
+};
